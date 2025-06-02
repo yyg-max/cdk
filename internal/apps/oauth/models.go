bc30fcbd6b826f832720b8f06707ad83602144f4
U8F69 <41963680+U8F69@users.noreply.github.com>
2025-06-02 16:17:45 +0800

    fix(oauth): model index invalid

--- internal/apps/oauth/models.go
+++ internal/apps/oauth/models.go
@@ -13,12 +13,12 @@ type OAuthUserInfo struct {
 
 type User struct {
 	ID          uint64     `json:"id" gorm:"primaryKey"`
-	Username    string     `json:"username" gorm:"unique,size:255"`
+	Username    string     `json:"username" gorm:"size:255;unique"`
 	Nickname    string     `json:"nickname" gorm:"size:255"`
 	AvatarUrl   string     `json:"avatar_url" gorm:"size:255"`
 	IsActive    bool       `json:"is_active" gorm:"default:true"`
 	TrustLevel  TrustLevel `json:"trust_level"`
-	LastLoginAt time.Time  `json:"last_login_at"`
-	CreatedAt   time.Time  `json:"created_at" gorm:"autoCreateTime,index"`
-	UpdatedAt   time.Time  `json:"updated_at" gorm:"autoUpdateTime"`
+	LastLoginAt time.Time  `json:"last_login_at;index"`
+	CreatedAt   time.Time  `json:"created_at" gorm:"autoCreateTime;index"`
+	UpdatedAt   time.Time  `json:"updated_at" gorm:"autoUpdateTime;index"`
 }
