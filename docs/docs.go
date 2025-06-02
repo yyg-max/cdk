4e8685715924eca8d4a6c737cf2fef641721615b
U8F69 <41963680+U8F69@users.noreply.github.com>
2025-06-02 16:00:17 +0800

    docs(swagger): update api doc

--- docs/docs.go
+++ docs/docs.go
@@ -193,7 +193,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
+	Version:          "0.1.0",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/linux-do/cdk/internal/cmd"
 
 // @title Linux Do CDK
+// @version 0.1.0
 func main() {
 	cmd.Execute()
 }
